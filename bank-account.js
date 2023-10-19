cad407f39d05b7caf573b3ec74c13e5a61e9bcb8
BomScoob <sorawitkraukham@gmail.com>
2023-10-19 10:36:54 +0700

    update bank/ pass all test

--- bank-account.js
+++ bank-account.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class BankAccount {
   constructor() {
     this._balance
